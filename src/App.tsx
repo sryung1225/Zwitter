0f4a688ef2f0ff0352364269a8b09e25c22217db
sryung <sryung1225@gmail.com>
2023-11-29 23:55:08 +0900

    [🥁 : feat] Routing
    - Outlet을 이용한 경로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/layout.tsx';
+import Home from './routes/home.tsx';
+import Profile from './routes/profile.tsx';
+import Login from './routes/login.tsx';
+import CreateAccount from './routes/create-account.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
