51082338cd36f08717b9ea20a1dcca2d8669ed5a
sryung <sryung1225@gmail.com>
2023-12-03 17:55:44 +0900

    [🥁 : feat] 로그아웃 버튼
    - 로그인되어있는 현재 유저를 로그아웃 시키는 firebase의 auth.signOut을 이용

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,13 @@
+import auth from '../firebase.ts';
+
 export default function Home() {
-  return <h1>Home!</h1>;
+  const logOut = () => {
+    auth.signOut();
+  };
+  return (
+    <>
+      <h1>Home!</h1>
+      <button onClick={logOut}>LogOut</button>
+    </>
+  );
 }
