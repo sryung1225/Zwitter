d37a72052bfcdd3735a0bc883070104601a819cb
sryung <sryung1225@gmail.com>
2023-12-09 01:33:48 +0900

    [💎 : refactor] onSnapshot 제한 걸기
    - onSnapshot은 쿼리 변화가 있을 때마다 실시간으로 이벤트 콜백을 진행해줌. 그래서 앞서 실시간 반영을 위해 사용했었음. 이 특성상 onSnapshot을 이용하고 있는 동안에 계속 read되어 비용이 지불됨
    => 유저가 다른 화면을 보고 있을 동안에는 작동하지 않도록 해주면 좋음
    
    - fetchTweets와 onSnapshot 과정을 useEffect 내부로 이동시켜줌
    - 컴포넌트가 unmount될 때 비활성시켜주는 useEffect의 cleanup 기능을 활용. 사용자가 로그아웃하거나 다른 화면으로 이동한 경우, 구독을 해제하는 함수를 반환하도록 함
    
    +) 비용 절감을 위해 화면에 불러올 트위터의 갯수도 제한해줌. 추후 페이지네이션 활용을 권장

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import styled from 'styled-components';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { Unsubscribe } from 'firebase/auth';
 import { db } from '../firebase.ts';
 import Tweet from './tweet.tsx';
 
@@ -13,38 +19,43 @@ export interface ITweet {
   photo: string;
 }
 
-const Wrapper = styled.div``;
-
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, 'tweets'),
-      orderBy('createdAt', 'desc'),
-    );
-    await onSnapshot(tweetsQuery, (snapshot) => {
-      const tweetList = snapshot.docs.map((doc) => {
-        const { userId, userName, tweet, createdAt, photo } = doc.data();
-        return {
-          userId,
-          userName,
-          tweet,
-          createdAt,
-          photo,
-          id: doc.id,
-        };
-      });
-      setTweets(tweetList);
-    });
-  };
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, 'tweets'),
+        orderBy('createdAt', 'desc'),
+        limit(25),
+      );
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweetList = snapshot.docs.map((doc) => {
+          const { userId, userName, tweet, createdAt, photo } = doc.data();
+          return {
+            userId,
+            userName,
+            tweet,
+            createdAt,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweets(tweetList);
+      });
+    };
     fetchTweets();
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
   }, []);
   return (
-    <Wrapper>
+    <>
       {tweets.map((tweet) => (
         <Tweet key={tweet.id} {...tweet} />
       ))}
-    </Wrapper>
+    </>
   );
 }
