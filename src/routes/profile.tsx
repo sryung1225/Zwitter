702af074aea8a9c570ece00519875d11c558c615
sryung <sryung1225@gmail.com>
2024-01-09 19:03:46 +0900

    [🥁 : feat] 개인 유저별 프로필 페이지 작업 중 (#2)

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import WindowTop from '@compo/window-top.tsx';
 import UserProfile from '@compo/profile/user-profile.tsx';
 import UserTimeline from '@compo/profile/user-timeline.tsx';
 import * as W from '@style/window.ts';
 
 export default function Profile() {
+  const location = useLocation();
+  const userFromLocation = location.search.slice(7);
+  console.log(userFromLocation);
+  useEffect(() => {
+    /* getDocs 로직 이어서 작성해야 함 */
+  }, [userFromLocation]);
+
   return (
     <W.Window>
       <WindowTop />
