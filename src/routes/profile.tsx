10dde187274941e849823440b2293bb50fb151ca
sryung <sryung1225@gmail.com>
2024-01-30 17:22:21 +0900

    [🛠️ : fix] 불필요한 코드 제거

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/firebase.ts';
 import WindowTop from '@compo/window-top.tsx';
 import UserProfile from '@compo/profile/user-profile.tsx';
 import UserTimeline from '@compo/profile/user-timeline.tsx';
-import currentUserAtom from '@atom/current-user.tsx';
 import IUser from '@type/IUser.ts';
 import * as W from '@style/window.ts';
 
@@ -14,7 +12,6 @@ export default function Profile() {
   const location = useLocation();
   const userFromLocation = location.search.slice(7);
   const [user, setUser] = useState<IUser | undefined>();
-  const currentUser = useRecoilValue(currentUserAtom);
   useEffect(() => {
     const fetchUser = async () => {
       const usersQuery = query(
@@ -22,15 +19,11 @@ export default function Profile() {
         where('userId', '==', userFromLocation),
       );
       const snapshot = await getDocs(usersQuery);
-      if (!snapshot.empty) {
-        const userData = snapshot.docs[0].data() as IUser;
-        setUser(userData);
-      } else {
-        console.error("can't find user data");
-      }
+      const userData = snapshot.docs[0].data() as IUser;
+      setUser(userData);
     };
     fetchUser();
-  }, [userFromLocation, currentUser]);
+  }, [userFromLocation]);
   return (
     <W.Window>
       <WindowTop />
