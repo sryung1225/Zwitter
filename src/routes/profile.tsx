9e468f3c0cd839fc3d3bebc162da7fd781e7f2d4
sryung <sryung1225@gmail.com>
2024-01-31 02:11:28 +0900

    [🛠 : fix] 프로필 자동 업데이트 기능 복구
    - 지난 작업에서 착오로 삭제한 코드 복구(10dde18) useEffect의 dependency로 현재 유저 데이터를 지정해 프로필 업데이트 시 리렌더링 기능 활성화

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/firebase.ts';
 import WindowTop from '@compo/window-top.tsx';
 import UserProfile from '@compo/profile/user-profile.tsx';
 import UserTimeline from '@compo/profile/user-timeline.tsx';
+import currentUserAtom from '@atom/current-user.tsx';
 import IUser from '@type/IUser.ts';
 import * as W from '@style/window.ts';
 
@@ -12,6 +14,7 @@ export default function Profile() {
   const location = useLocation();
   const userFromLocation = location.search.slice(7);
   const [user, setUser] = useState<IUser | undefined>();
+  const currentUser = useRecoilValue(currentUserAtom);
   useEffect(() => {
     const fetchUser = async () => {
       const usersQuery = query(
@@ -23,7 +26,7 @@ export default function Profile() {
       setUser(userData);
     };
     fetchUser();
-  }, [userFromLocation]);
+  }, [userFromLocation, currentUser]);
   return (
     <W.Window>
       <WindowTop />
